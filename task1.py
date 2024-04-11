8e5d58143ffb661cc471f5fe91af7e0f345a786b
sanjanachecker <sanjanachecker@gmail.com>
2024-04-11 12:47:52 -0700

    new

--- task1.py
+++ task1.py
@@ -1,3 +1,4 @@
+from ssl import RAND_bytes
 import sys
 from PIL import Image
 from Crypto.Cipher import AES
@@ -11,9 +12,15 @@ im.show()
 def ecb(im):
     # need to ensure that file is evenly disible by 128-bits with PKCS#7 padding
     # then encrypt 128 bits at a time
-    # key =
+    key = RAND_bytes(16)
     cipher = AES.new(key, AES.MODE_ECB)
+
+    print('key = ', key)
+    print(cipher)
 
 
 def cbc(im):
     pass
+
+
+ecb(im)
